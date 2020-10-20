c9b0f446a8846e01e18988dac6cd1f1fa910d239
User <User@ES301N5>
2020-10-20 15:56:49 +0800

    loooop

---
+++ src/com/juan/Loop.java
@@ -0,0 +1,18 @@
+package com.juan;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while(i < 5) {
+			System.out.println(i);
+			i++;
+			if(i == 3) {
+				break;
+			}
+		}
+		System.out.println("Done");
+
+	}
+
+}
